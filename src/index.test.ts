84a2d71c779335ca461849070a779a359d95aee3
Daniela Garzon <digarzonp@unal.edu.co>
2023-07-03 09:05:02 -0500

    v 0.0.1.3

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,35 +1,32 @@
-//importat las bibliotecas 
-
-import {Request} from 'supertest'
+import request from 'supertest'
 import App from './Router'
-import { request } from 'http'
 
 describe(
 	'GET /',
 	()=>{
         
-		let app:App
-
+		let app:App        
+        
 		beforeAll(
 			()=>{
-				app= new App()
-				app.start
+				app=new App()
+				app.start()
 			}
 		)
-        
+
 		afterAll(
-			app.close
+			()=>{
+				app.close()
+			}           
 		)
 
 		test(
 			'Debe devolver un mensaje',
 			async ()=>{
-				const res = await request(app.app).get('./') 
-				expect(res.statusCode).toEqual(200)
-				expect(res.test).toEqual('Bienvenidos a typescript')     
+				const res = await request(app.app).get('/')
+				expect (res.statusCode).toEqual(200)
+				expect (res.text).toEqual('Bienvenidos a typescript')
 			}
 		)
-
 	}
-
 )
